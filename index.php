041c57808c1c32ddc0aaaeb5f99763c37cf440bb
Heather N. Larsen <heatherlarsen758@u.boisestate.edu>
2019-09-21 16:18:00 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="global.css"/>
+    </head>
+    <body>
+        <?php require_once "./header-nav.html" ?>
+        <img id="home-banner" src="img/home_banner.png"/>
+        <?php require_once "./footer.html" ?>
+    </body>
+</html>
