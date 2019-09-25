07cc8c2726e08f9382c1b9aec0e8d4b1bf84897a
Heather N. Larsen <heatherlarsen758@u.boisestate.edu>
2019-09-25 16:16:03 -0600

    Added registration page

--- log-in.php
+++ log-in.php
@@ -5,8 +5,22 @@
     </head>
     <body>
         <?php require_once "./header-nav.html" ?>
-        <div>
-            <h1>Log In</h1>
+        <div id="log-in-page">
+            <h1 class="center">Log In</h1>
+            <div class="form1" id="log-in">
+                <div id="log-in-fields">
+                    <label for="email">E-mail Address</label>
+                    <input id="email" type="text" placeholder="E-mail Address"/>
+                    <label for="passwd">Password</label>
+                    <input id="passwd" type="text" placeholder="Password"/>
+                </div>
+                <div id="log-in-submit">
+                    <input type="submit" value="Log In"/>
+                </div>
+            </div>
+            <div class="registration-link"><a href="sign-up.php">
+                Don't have an account? Sign up here.
+            </a></div>
         </div>
         <?php require_once "./footer.html" ?>
     </body>
---
+++ sign-up.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="global.css"/>
+    </head>
+    <body>
+        <?php require_once "./header-nav.html" ?>
+        <div id="sign-up-page">
+            <h1 class="center">Sign Up</h1>
+            <div class="form1" id="sign-up">
+                <div class="col2-left">
+                    <label for="name">Your Name</label>
+                    <input id="name" type="text" placeholder="Your Name"/>
+                    <label for="passwd">Password</label>
+                    <input id="passwd" type="text" placeholder="Password"/>
+                </div>
+                <div class="col2-right">
+                    <label for="email">E-mail Address</label>
+                    <input id="email" type="text" placeholder="E-mail Address"/>
+                    <label for="conf-passwd">Confirm Password</label>
+                    <input id="conf-passwd" type="text" placeholder="Confirm Password"/>
+                </div>
+                <div id="sign-up-submit">
+                    <input type="submit" value="Sign Up"/>
+                </div>
+            </div>
+            <div class="registration-link"><a href="log-in.php">
+                Already have an account? Log in here.
+            </a></div>
+        </div>
+        <?php require_once "./footer.html" ?>
+    </body>
+</html>
