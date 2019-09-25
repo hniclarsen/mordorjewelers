8a087d2707e02dd967abe92460eb73130e2fc490
Heather N. Larsen <heatherlarsen758@u.boisestate.edu>
2019-09-25 13:38:04 -0600

    Periodic commit

--- index.php
+++ index.php
@@ -6,6 +6,36 @@
     <body>
         <?php require_once "./header-nav.html" ?>
         <img id="home-banner" src="img/home_banner.png"/>
+        <div>
+            <div class="featured-item">
+                <img class="round-img"/>
+                <div class="item-text">
+                    <div class="item-name">Featured Item</div>
+                    <div class="item-price">Price</div>
+                </div>
+            </div>
+            <div class="featured-item">
+                <div class="round-img"></div>
+                <div class="item-text">
+                    <div class="item-name">Featured Item</div>
+                    <div class="item-price">Price</div>
+                </div>
+            </div>
+            <div class="featured-item">
+                <div class="round-img"></div>
+                <div class="item-text">
+                    <div class="item-name">Featured Item</div>
+                    <div class="item-price">Price</div>
+                </div>
+            </div>
+            <div class="featured-item">
+                <div class="round-img"></div>
+                <div class="item-text">
+                    <div class="item-name">Featured Item</div>
+                    <div class="item-price">Price</div>
+                </div>
+            </div>
+        </div>
         <?php require_once "./footer.html" ?>
     </body>
 </html>
