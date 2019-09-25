f9d3a05d8286a156120d53945a8a028f70e9ad0c
Heather N. Larsen <heatherlarsen758@u.boisestate.edu>
2019-09-25 14:04:37 -0600

    Started log-in page

---
+++ log-in.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="global.css"/>
+    </head>
+    <body>
+        <?php require_once "./header-nav.html" ?>
+        <div>
+            <h1>Log In</h1>
+        </div>
+        <?php require_once "./footer.html" ?>
+    </body>
+</html>
