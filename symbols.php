4dd5ae5f5eef95c4d02c5e41ec966ec51446068a
Heather N. Larsen <heatherlarsen758@u.boisestate.edu>
2019-09-26 21:33:39 -0600

    Periodic commit

---
+++ symbols.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="global.css"/>
+    </head>
+    <body>
+        <?php require_once "./header-nav.html" ?>
+        <div>
+            <h1 class="center">Symbols</h1>
+            <div>
+                <div class="round-img">
+                </div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+        </div>
+        <?php require_once "./footer.html" ?>
+    </body>
+</html>
