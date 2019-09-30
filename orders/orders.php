d0860dc684fa249e08c71771803c023f2ed41167
Heather N. Larsen <heatherlarsen758@u.boisestate.edu>
2019-09-30 16:16:42 -0600

    Added Orders page

--- orders/orders.php
+++ orders/orders.php
@@ -11,6 +11,49 @@
         <div id="orders-page" class="center">
             <h1>Orders</h1>
             <hr/>
+            <div>
+                <div>
+                    <div class="order-header">
+                        <span class="order-placed">Order Placed</span>
+                        <span class="total">Total</span>
+                        <span id="order-num">Order #</span>
+                    </div>
+                    <div class="order-information">
+                        <span class="order-placed">Month Day, Year</span>
+                        <span class="total">Price</span>
+                    </div>
+                    <div>
+                        <div class="product-information">
+                            <img class="box-img"/>
+                            <div class="product-details">
+                                <div id="product-name">Product Name</div>
+                                <div id="product-price">Price</div>
+                                <input type="submit" value="Buy It Again"/>
+                                <input id="view-item" type="submit" value="View Item"/>
+                            </div>
+                        </div>
+                        <div class="product-information">
+                            <img class="box-img"/>
+                            <div class="product-details">
+                                <div id="product-name">Product Name</div>
+                                <div id="product-price">Price</div>
+                                <input type="submit" value="Buy It Again"/>
+                                <input id="view-item" type="submit" value="View Item"/>
+                            </div>
+                        </div>
+                        <div class="product-information">
+                            <img class="box-img"/>
+                            <div class="product-details">
+                                <div id="product-name">Product Name</div>
+                                <div id="product-price">Price</div>
+                                <input type="submit" value="Buy It Again"/>
+                                <input id="view-item" type="submit" value="View Item"/>
+                            </div>
+                        </div>
+                    </div>
+                    <hr/>
+                </div>
+            </div>
         </div>
         <?php require_once "../footer.html" ?>
     </body>
