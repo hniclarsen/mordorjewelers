d04e6a52f0a889ab3f63a2d92978653b6a3576a4
Heather N. Larsen <heatherlarsen758@u.boisestate.edu>
2019-11-05 12:21:02 -0700

    test

--- dao.php
+++ dao.php
@@ -62,7 +62,7 @@ class Dao {
             $results = $stmt->fetchAll();
             $valid = password_verify($password, $results[0]['password']);
             
-            if($valid) return $results[0];
+            if($valid === true) return $results[0];
             else return null;
         } catch(Exception $e) {
             $this->logger->LogInfo("Failed to retrieve user: {$e}");
