6cf0b29d74b91e93c3f2faa445f55fed357adfa9
Heather N. Larsen <heatherlarsen758@u.boisestate.edu>
2019-11-05 12:18:20 -0700

    test

--- login/log-in-handler.php
+++ login/log-in-handler.php
@@ -4,13 +4,13 @@ session_start();
 require_once "../dao.php";
 $dao = new Dao();
 $userUUID = $dao->getUser($_POST['email'], $_POST['passwd']);
-if(is_null($userUUID)) {
-    $_SESSION['message'] = "Invalid login credentials.";
-    header("Location: /login/log-in.php");
-} else {
+if(!empty($userUUID)) {
     $_SESSION['userUUID'] = $userUUID['userUUID'];
     $_SESSION['username'] = $userUUID['name'];
     $_SESSION['sentiment'] = 'OK';
     header("Location: /");
+} else {
+    $_SESSION['message'] = "Invalid login credentials.";
+    header("Location: /login/log-in.php");
 }
 ?>
