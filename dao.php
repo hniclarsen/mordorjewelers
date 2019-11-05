cd306432a6e52fb940eeca5ac4e878c7c3195164
Heather N. Larsen <heatherlarsen758@u.boisestate.edu>
2019-11-05 12:03:47 -0700

    Handle invalid login credentials

--- dao.php
+++ dao.php
@@ -63,6 +63,7 @@ class Dao {
             $valid = password_verify($password, $results[0]['password']);
             
             if($valid) return $results[0];
+            else return null;
         } catch(Exception $e) {
             $this->logger->LogInfo("Failed to retrieve user: {$e}");
             exit;
--- login/log-in-handler.php
+++ login/log-in-handler.php
@@ -1,14 +1,16 @@
 <?php
 session_start();
 
-$presets = $_POST;
-
 require_once "../dao.php";
 $dao = new Dao();
 $userUUID = $dao->getUser($_POST['email'], $_POST['passwd']);
-$_SESSION['userUUID'] = $userUUID['userUUID'];
-$_SESSION['username'] = $userUUID['name'];
-
-$_SESSION['sentiment'] = 'OK';
-header("Location: /");
+if($userUUID) {
+    $_SESSION['userUUID'] = $userUUID['userUUID'];
+    $_SESSION['username'] = $userUUID['name'];
+    $_SESSION['sentiment'] = 'OK';
+    header("Location: /");
+} else {
+    $_SESSION['message'] = "Invalid login credentials.";
+    header("Location: /login/log-in.php");
+}
 ?>
